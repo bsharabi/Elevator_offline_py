0d32b4b1df6e79fb660c8fef7b1de76276fd520e
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2021-11-17 23:26:12 +0200

    push all

--- Building.py
+++ Building.py
@@ -1,9 +1,14 @@
+from Calls import calls
 from Elevator import elevator
 class build:
     # __init__ - is a keyword to the class constructor
-    def __init__(self, obj):
+    def __init__(self, obj,callsList):
         self._minFloor = obj["_minFloor"]
         self._maxFloor = obj["_maxFloor"]
         self._elevators = []
+        self._calls=[]
         for e in obj["_elevators"]:
             self._elevators.append(elevator(e))
+        for c in callsList:
+            self._calls.append(calls(c))
+        
--- Calls.py
+++ Calls.py
@@ -1,11 +1,15 @@
 class calls:
-    id=0
-       # __init__ - is a keyword to the class constructor
-    def __init__(self, listCall):
-        self.name=  listCall[0] + str(calls.id)
-        self.timeIn=float(listCall[1])
-        self.src =int(listCall[2])
-        self.dest=int(listCall[3])
-        self.allocateTo=int(listCall[5])
-        calls.id +=1 
-    pass
+
+    # __init__ - is a keyword to the class constructor
+    def __init__(self, Call):
+        self.name = Call[0]
+        self.timeIn = float(Call[1])
+        self.src = int(Call[2])
+        self.dest = int(Call[3])
+        self.allocateTo = int(Call[5])
+        self.flag = int(Call[4])
+
+    def toString(self):
+        return str(self.name),str(self.timeIn),str(self.src),str(self.dest),str(self.flag),str(self.allocateTo)
+    
+    

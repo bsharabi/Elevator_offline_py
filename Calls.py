6a7c0f58317343a03b1ea48619a01fae1d507f21
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2021-11-18 18:00:29 +0200

    new allocate function

--- Calls.py
+++ Calls.py
@@ -1,5 +1,5 @@
 class calls:
-
+    id=0
     # __init__ - is a keyword to the class constructor
     def __init__(self, Call):
         self.name = Call[0]
@@ -8,8 +8,12 @@ class calls:
         self.dest = int(Call[3])
         self.allocateTo = int(Call[5])
         self.flag = int(Call[4])
+        calls.id +=1 
+        self.diraction = ((self.dest-self.src)>0);
 
     def toString(self):
         return str(self.name),str(self.timeIn),str(self.src),str(self.dest),str(self.flag),str(self.allocateTo)
     
+    def allocateTo(self,numberElevator):
+        self.allocateTo=numberElevator;
     
